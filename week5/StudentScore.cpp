1c165bc20efde04e5ef8273f4f9d5b6898a6e076
Nguyen Hoang Gia Bao <giabao.110207@gmail.com>
2026-09-09 10:34:02 +0700

    Update StudentScore

--- week5/StudentScore.cpp
+++ week5/StudentScore.cpp
@@ -14,5 +14,17 @@ while (n < 2 || n > 20)
     cin >> n;
 }
 
+//Enter the scores of the students
+double scores[20];
+for (int i = 0; i < n; i++){
+    cout << "Enter the score of student " << i + 1 << ": ";
+    cin >> scores[i];
+
+while (scores[i] < 0 || scores[i] > 10)
+    {
+        cout << "Invalid score! Please enter a value from 0 to 10: ";
+        cin >> scores[i];
+    }
+}
 return 0;
 }
